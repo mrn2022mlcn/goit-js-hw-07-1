9adeb1f82ae9e522ca201688619cfb7a80fc01f1
Marina Omelcenko <98337926+mrn2022mlcn@users.noreply.github.com>
2022-08-13 01:20:43 +0300

    update gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -6,6 +6,8 @@ const galleryMarkup = createGalleryMarkup(galleryItems);
 
 galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
 
+galleryContainer.addEventListener('click', onGalleryContainerClick)
+
 function createGalleryMarkup(galleryItems) {
     return galleryItems
     .map(({preview, original, description }) => {
@@ -22,5 +24,33 @@ function createGalleryMarkup(galleryItems) {
 })
 .join('');
 }
+
+function onGalleryContainerClick(evt) {
+if(!evt.target.classlist.contains('gallery__image')) {
+    return;
+}
+console.log(evt.target);
+}
+evt.preventDefault();
+onModalOpen(evt);
+console.log(evt.target.dataset.source);
+
+function onModalOpen(evt) {
+    const instance = basicLightbox.create (
+        `img src = '${evt.target.dataset.source}' width = "1280"
+        `
+        );
+    instance.show();
+    addEventListener('keydown', onEscapePress);
+
+function onEscapePress(evt) {
+    const ESC_KEY_CODE = 'Escape';
+    const isEscape = evt.code === ESC_KEY_CODE;
+    if (isEscape) {
+        instance.close();
+    }
+}
+}
+
 
 console.log(galleryItems);

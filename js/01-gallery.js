9a1690ac84ad312b718bbc65cf9cc0e1cb78af0a
Marina Omelcenko <98337926+mrn2022mlcn@users.noreply.github.com>
2022-08-13 00:10:05 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,17 +1,22 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-const markup = createGalleryMarkup;
-console.log(markup);
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
 
 function createGalleryMarkup(galleryItems) {
-    return galleryItems.map(({preview, original, description }) => {
+    return galleryItems
+    .map(({preview, original, description }) => {
    return `
    <div class = "gallery__item"> 
    <a class = "gallery__link" href '${original}'>
    <img class = "gallery__image" 
    src = '${preview}'
+   data-source = '${original}'
    alt = '${description}'/>
-   />
+   </a>
    </div>
    `
 })
